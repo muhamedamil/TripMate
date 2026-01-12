74235e6f63d031631c54247b46b1d9579567c0ee
Muhammed_amil <amilasnils008@gmail.com>
2026-01-12 12:22:50 +0530

    added the logic for the utlis currency converter and expense calculator

---
+++ utils/expense_calculator.py
@@ -0,0 +1,39 @@
+#Class calculator
+
+class calculator:
+    @staticmethod
+    def multiply(a: int, b: int) -> int:
+        """Multiply two integers
+
+        Args:
+            a (int): first integer
+            b (int): second integer
+
+        Returns:
+            int:the product of a and b
+        """
+        
+    @staticmethod
+    def calculate_tool(*x: float) -> float:
+        """Calculate sum of the given list of numbers
+
+        Args:
+            X(list): List of floating numbers
+        Returns:
+            float: The sum of numbers in the list X
+        """
+        return sum(x)
+    
+    @staticmethod
+    def calculate_daily_budget(total: float, days: int) -> float:
+        """Calculate the daily budget 
+
+        Args:
+            total (float): Total cost
+            days (int): Total days
+
+        Returns:
+            float: Expense for the single day
+        """
+        return total / days if days > 0 else 0 
+        
