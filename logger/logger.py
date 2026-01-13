29155377341ba4a299efbf47b5b8f50e7dbeffb8
Muhammed_amil <amilasnils008@gmail.com>
2026-01-13 13:31:20 +0530

    updated the logger functionality

---
+++ logger/logger.py
@@ -0,0 +1,25 @@
+import logging
+import os
+from datetime import datetime
+
+
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(os.path.dirname(logs_path), exist_ok=True)
+
+logging.basicConfig(
+    filename=logs_path,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+)
+console.setFormatter(formatter)
+logging.getLogger("").addHandler(console)
+
+logger = logging.getLogger("trip_mate")
